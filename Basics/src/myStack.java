27f73aa25ed009b8e334a5ec417721ab9afba2f9
AbigailParrales <danielaparralesd.m@gmail.com>
2017-08-19 15:54:08 -0500

    Making the stack work

--- Basics/src/myStack.java
+++ Basics/src/myStack.java
@@ -1,46 +1,79 @@
+import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
 
 public class myStack <N>{
 	private Node top;
-	private int length;
-	
+
 	public static class Node {
-		public int key;
 		public Comparable data;
 		public Node next;
-		
+
 		public Node (Comparable data) {
 			this.data=data;
 		}
-}
+
+		public Node() {
+			this.data = "";
+			this.next = new Node(0);
+		}
+		
+		
+	}
 
 	public void push(Comparable data) {
 		Node node = new Node(data);
-		node.next= top;
-		length++;
-	}
-	
-	public void pop(){
-		top = top.next;
-	}
-	
-	public Node peek() {
-		return top;
-	}
-	
-	public Boolean isEmpty() {
-		return top==null;
-	}
-	
-	public String toString() {
-		String tmp="";
-		Node copy= new Node(0);
-		
-		for(int i = 0;i>length;i++) {
-			tmp+=top.data;
+		if (this.isEmpty()) {
+			//System.out.println("estoy en el if del push");
+			node.next=null;
+			this.top=node;
+		}
+		else {
+			//System.out.println("estoy en el else del push");
 			
+			//System.out.println("haciendo el top el nuevo next de node");
+			
+			node.next=this.top;
+			
+			//System.out.println("haciendo top igual a node");
+			
+			this.top=node;
+			//System.out.println("este es el elemento top: "+ this.top);
+		}
+	}
+
+	public void pop(){
+		this.top = this.top.next;
+	}
+
+	public Comparable peek() {
+		return this.top.data;
+	}
+ 
+	public Boolean isEmpty() {
+		return this.top==null;
+	}
+
+	public String toString() {
+		System.out.println("estoy en toString()\n");
+		
+		String tmp="";
+		
+		System.out.println("hice una copia de top");
+		Node copy= new Node(0);
+		copy=this.top;
+		
+		while (isEmpty()==true){
+			System.out.println("Estoy en el while");
+			tmp=copy.data+", ";
+			copy=copy.next;
+			System.out.println("tmp actualizada= "+ tmp );
+			System.out.println("nuva data del copy: "+ copy.data+"");
 		}
 		
-		return null;
+		System.out.println("Salí del while");
+		System.out.println("Este es el tmp que regresaré: "+tmp);
+		System.out.println("Saliendo de toString()\n");
+		
+		return tmp;
 	}
-	
+
 }
--- Basics/src/testMyStack.java
+++ Basics/src/testMyStack.java
@@ -7,53 +7,60 @@ public class testMyStack {
 	public static void main(String[] args) {
 
 		myStack myStack= new myStack<Integer>();
-
 		Scanner sc = new Scanner(System.in);
+		while(true) {
+			System.out.println("Introduzca una de las siguientes opciones: \n"
+					+ "1) Insertar\n"
+					+ "2) Eliminar\n"
+					+ "3) Mostrar el elemento top\n"
+					+ "4) Mostrar todo el Stack"
+					//+ "---------------------------------------------------------------------\n"
+					+ "\n");
 
-		System.out.println("Introduzca una de las siguientes opciones: \n"
-				+ "1) Insertar\n"
-				+ "2) Eliminar\n"
-				+ "3) Mostrar el elemento top\n"
-				+ "4) Mostrar todo el Stack");
+			int opcion = Integer.parseInt(sc.nextLine());
 
-		int opcion = Integer.parseInt(sc.nextLine());
-
-		switch (opcion) {
-		case 1:
-			System.out.println("Introduzca un número");
-			int num = Integer.parseInt(sc.nextLine());
-			System.out.println("Se introdujo el número");
-			break;
-		case 2:
-			if(myStack.isEmpty()) {
-				System.out.println("El Stack está vacio");
+			switch (opcion) {
+			case 1:
+				System.out.println("Introduzca un número"+"\n");
+				int num = Integer.parseInt(sc.nextLine());
+				myStack.push(num);
+				System.out.println("Se introdujo el número: "+num+"\n");
+				break;
+			case 2:
+				if(myStack.isEmpty()) {
+					System.out.println("El Stack está vacio"+"\n");
+				}
+				else {
+					myStack.pop();
+					System.out.println("Se ha elimidado el elemento top"+"\n");
+				}
+				break;
+			case 3:
+				if(myStack.isEmpty()) {
+					System.out.println("El Stack está vacio"+"\n");
+				}
+				else {
+					System.out.println("El elemento top es: "+ myStack.peek()+"\n");
+				}
+				break;
+			case 4:
+				System.out.println("entré al case 4");
+				if(myStack.isEmpty()) {
+					System.out.println("El Stack está vacio"+"\n");
+				}
+				else {
+					System.out.println("entré al else del case 4");
+					
+					myStack.toString();
+					//System.out.println("myStack= "+ myStack+"\n");
+					
+					System.out.println("terminé el else del case 4");
+				}  
+				//System.out.println(myStack.toString());
+				break;
+			default:
+				break;
 			}
-			else {
-				myStack.pop();
-				System.out.println("Se ha elimidado el elemento top");
-			}
-			break;
-		case 3:
-			if(myStack.isEmpty()) {
-				System.out.println("El Stack está vacio");
-			}
-			else {
-				System.out.println("El elemento top es: "+ myStack.peek());
-			}
-			break;
-		case 4:
-			if(myStack.isEmpty()) {
-				System.out.println("El Stack está vacio");
-			}
-			else {
-				String tmp="";
-				//for(int i = 0;i>myStack.length;i++) {
-
-			}
-
-			break;
-		default:
-			break;
 		}
 	}
 }
