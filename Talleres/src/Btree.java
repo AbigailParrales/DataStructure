5449ea984abd32363bfe04041397a883797c77b0
Aby <danielaparralesd.m@gmail.com>
2017-11-15 13:26:07 -0600

    things

--- Talleres/src/Btree.java
+++ Talleres/src/Btree.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 
 import javax.swing.JOptionPane;
 
-public class Btree {
+public class BTree {
 	private int t;
 	private BNode root;
 
@@ -27,7 +27,7 @@ public class Btree {
 		}
 	}
 
-	public Btree(int t) {
+	public BTree(int t) {
 		this.t=t;
 		this.root= new BNode(this.t);
 		write_disk(root,"root.ptr");
@@ -39,7 +39,7 @@ public class Btree {
 
 	private File write_disk(BNode nodo, String name) {
 		BufferedWriter writer = null;
-		File f= new File("C:\\Users\\oscar\\Desktop\\pruebaBTree\\"+name);
+		File f= new File("C:\\Users\\Daniela Parrales\\Documents\\GitHub\\DataStructure\\Talleres\\TextFiles_tmp\\"+name);
 		try {
 			writer = new BufferedWriter(new FileWriter(f));
 			if(nodo.ifLeaf) {
@@ -82,7 +82,7 @@ public class Btree {
 		String linea="";
 
 		try {
-			BufferedReader bf = new BufferedReader(new FileReader("C:\\Users\\oscar\\Desktop\\pruebaBTree\\"+archivo));
+			BufferedReader bf = new BufferedReader(new FileReader("C:\\Users\\Daniela Parrales\\Documents\\GitHub\\DataStructure\\Talleres\\TextFiles_tmp\\"+archivo));
 			int i=0;
 			int even=0;
 			int odd=0;
@@ -118,8 +118,8 @@ public class Btree {
 				}
 				i++;
 			}
-			System.out.println(".ptr: "+Arrays.toString(c_tmp));
-			System.out.println(".key: "+Arrays.toString(x_tmp));
+			//System.out.println(".ptr: "+Arrays.toString(c_tmp));
+			//System.out.println(".key: "+Arrays.toString(x_tmp));
 			BNode nodo= new BNode(this.t);
 			nodo.c=c_tmp;
 			nodo.x=x_tmp;
@@ -165,10 +165,13 @@ public class Btree {
 	}
 
 	public void splitChild(BNode x, int i) {
-		System.out.println("In split");
 		BNode z = new BNode(t);
-		System.out.println(Arrays.toString(x.c));
-		System.out.println(i);
+		
+		System.out.println("-----------------X----------------");
+		System.out.println("Keys "+Arrays.toString(x.x));
+		System.out.println("Apuntadores "+Arrays.toString(x.c));
+		System.out.println("i= "+i);
+		
 		BNode y = this.read_disk(x.c[i-1]+".ptr");
 		z.ifLeaf=y.ifLeaf;
 		z.n=t-1;
@@ -202,16 +205,15 @@ public class Btree {
 	}
 
 	private void insertNonfull(BNode x, int k) {
-		
-		int i = x.n;/**/
+		int i = x.n;
 		if(x.ifLeaf) {
-			if(x.n==0){
+			if(i==0){
 				x.x[0]=k;
 				x.n=1;
 				write_disk(x, x.x[0]+".ptr");
-			}//**
+			}
 			else {
-				while(i>=1 && k<x.x[i-1]) {//*
+				while(i>=1 && k<x.x[i]) {
 					x.x[i]=x.x[i-1];
 					i--;
 				}
@@ -266,13 +268,13 @@ public class Btree {
 	}
 
 	public static void main(String[] args) {
-		Btree bt= new Btree(2);
+		BTree bt= new BTree(2);
+		bt.insert(2);
 		bt.insert(1);
-		bt.insert(3);
-		bt.insert(2);		
-		bt.insert(4);
-		bt.insert(5);
-		bt.insert(6);
+		//bt.insert(3);
+		//bt.insert(4);
+		//bt.insert(5);
+		//bt.insert(6);
 		//System.out.println("Children: "+Arrays.toString(bt.root.c));
 		//System.out.println("Keys: "+Arrays.toString(bt.root.x));
 	}
